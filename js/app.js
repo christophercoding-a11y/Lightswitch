9c04a32870e05ae2a9baf0e787f7a6584776bef2
christophercodinga11y <christopherjack14@gmail.com>
2024-12-06 12:05:49 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// How to program a light switch
+
+const onbutton = document.getElementById('onBtn')
+const offbutton = document.getElementById('offBtn')
+
+// console.log(onbutton)
+
+offbutton.addEventListener('click', ()=> {
+    console.log('click')
+})
+
